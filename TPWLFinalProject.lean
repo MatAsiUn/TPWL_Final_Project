8c79145ee084ae3a7e3fee8b1ba3f7b5cb5fe248
arplodge <arplodge@icloud.com>
2025-11-18 12:34:23 +0000

    Second comment Archie

--- TPWLFinalProject.lean
+++ TPWLFinalProject.lean
@@ -1,4 +1,8 @@
 import TPWLFinalProject.Basic
 import Mathlib.Tactic.Basic
+set_option linter.style.commandStart false
 
 -- Archie's first update
+
+
+-- This is the second comment
--- TestSecondFile.lean
+++ TestSecondFile.lean
@@ -2,3 +2,5 @@ import Mathlib.Tactic.Basic
 set_option linter.style.commandStart false
 
 -- This is a funky file from Mateo
+
+#check Nat.add_comm

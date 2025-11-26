5c901c3451cbb05e43b5bc9e4ac4b18524146ce6
arplodge <arplodge@icloud.com>
2025-11-26 16:35:05 +0000

    attempt 1 to formulate F_y

--- TPWL -Archie.lean
+++ TPWL -Archie.lean
@@ -4,13 +4,14 @@ import Mathlib.Analysis.InnerProductSpace.Defs
 set_option linter.style.commandStart false
 
 -- As in FA1, our first step to defining duals is to define what it means to be a linear functional
-def LinearFunctionalProp (K V : Type _) [Field K] [AddCommGroup V] [Module K V] (F: V → K) : Prop :=
+def linear_function_prop (K V : Type _) [Field K] [AddCommGroup V] [Module K V] (F: V → K) :=
   ∀ (x y : V) (a b : K), F (a • x + b • y) = a * (F x) + b * (F y)
 -- Note that "V" is our vector space here
 
+--PROBLEM
 -- Now define the linear functional Fy induced by a vector y in Hilbert space H.
+--variable {K H: Type _}[RCLike K][InnerProductSpace K H]{y : H}
+--def inner_product_functional (x: H) : K :=
+ --fun x => inner x y
 
---variable {K H: Type _}[RCLike K][InnerProductSpace K H]
-
---def inner_product_functional (y: H) : H →ₗ[K] K where
- -- Fun x := inner x y
+--theorem inner_prod_func_is_lin :

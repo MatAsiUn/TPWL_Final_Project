2e220d9304272834ffde1535e29fb24a64f457a1
arplodge <arplodge@icloud.com>
2025-12-02 15:57:51 +0000

    Add Trivial G to main file and update main file with comments

--- TPWL -Archie.lean
+++ TPWL -Archie.lean
@@ -11,17 +11,12 @@ import Mathlib.Analysis.Complex.Basic
 
 set_option linter.style.commandStart false
 
--- As in FA1, our first step to defining duals is to define what it means to be a linear functional
+-- We define what it means for a functional to be linear
 def linear_function_prop (K V : Type _) [Field K] [AddCommGroup V] [Module K V] (F: V → K) :=
   ∀ (x y : V) (a b : K), F (a • x + b • y) = a * (F x) + b * (F y)
 -- Note that "V" is our vector space here
 
---Definition Error
--- Now define the linear functional Fy induced by a vector y in Hilbert space H.
---variable {K H: Type _}[RCLike K][InnerProductSpace K H]{y : H}
---def inner_product_functional (x: H) : K :=
- --fun x => inner x y
-
+-- We want to prove Riesz for a
 open scoped ComplexInnerProductSpace
 variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℂ E]
 
--- TPWLFinalProject.lean
+++ TPWLFinalProject.lean
@@ -15,8 +15,35 @@ set_option linter.style.commandStart false
 --Mathlib.Analysis.NormedSpace does indeed not exist
 --Instead you need to import Mathlib.Analysis.Normed.Module
 --for results related to what we are doing
+
+-- We use a nested structure to differentiate between results that are applicable in
+-- an Inner Product Space (IPS), and ones only applicable in a Hilbert Space
+section inner_product_space_theorems
+
 open scoped ComplexInnerProductSpace
-variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℂ E] [CompleteSpace E]
+-- This is active for our entire file
+variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℂ E]
+
+ -- The statment of Riesz Representation Theorem for a trivial G (G identically 0)
+lemma Riesz_Representation_Theorem_TrivialG {x : E}(G: StrongDual ℂ E)(h: G x = 0):
+ G x = ⟪x,0⟫ := by
+ -- We use a lemma found in IPS.Basic, that tells us the inner
+ -- product of anything with 0 is 0
+ simp only [inner_zero_right]
+
+-- We can use exact as G x = 0, is what is left to prove but this was one of
+-- assumptions
+ exact h
+
+-- Section for Hilbert Spaces
+section hilbert_space_theorems
+
+-- We add the completeness assumption
+variable [CompleteSpace E]
+
 theorem Riesz_Representation_Theorem(G: StrongDual ℂ E):
  ∃! v : E, ∀ x : E, G x = ⟪x,v⟫ := by
  sorry
+
+end hilbert_space_theorems
+end inner_product_space_theorems
